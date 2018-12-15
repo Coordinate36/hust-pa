b8dfda5e3a6e526e67e71310ca3a8092f7a1387b
tracer-ics2018 <tracer@njuics.org>
2018-12-15 11:01:56 +0000

    >  compile U201514632 ics Linux fbe234129db9 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  11:01:55 up 3 days,  2:47,  3 users,  load average: 0.08, 0.07, 0.07 4584154969be8bbfc6b9e3f6b1911a621a460022

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -209,7 +209,7 @@ make_DHelper(SI2E) {
   decode_op_rm(eip, id_dest, true, NULL, false);
   id_src->width = 1;
   decode_op_SI(eip, id_src, true);
-  Log("after decode_op_SI\n");
+  Log("eip:%d. after decode_op_SI\n", *eip);
   if (id_dest->width == 2) {
     id_src->val &= 0xffff;
   }
--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -25,7 +25,7 @@ static inline void idex(vaddr_t *eip, opcode_entry *e) {
   /* eip is pointing to the byte next to opcode */
   if (e->decode)
     e->decode(eip);
-  Log("before execute!\n");
+  Log("eip:%d. before execute!\n", *eip);
   e->execute(eip);
 }
 
