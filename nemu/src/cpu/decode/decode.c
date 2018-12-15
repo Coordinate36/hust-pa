d4a3351c422e14930ffb12f954f34321adf63071
tracer-ics2018 <tracer@njuics.org>
2018-12-15 11:11:53 +0000

    >  compile U201514632 ics Linux fbe234129db9 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  11:11:52 up 3 days,  2:57,  3 users,  load average: 0.08, 0.09, 0.08 8cf3bbcc5ffcdaf4a6a78f806a33e159aea35b9e

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -205,6 +205,7 @@ make_DHelper(test_I) {
 }
 
 make_DHelper(SI2E) {
+  Log("eip:0x%x. before decode_op_SI\n", *eip);
   assert(id_dest->width == 2 || id_dest->width == 4);
   decode_op_rm(eip, id_dest, true, NULL, false);
   id_src->width = 1;
